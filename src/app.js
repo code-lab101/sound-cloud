b56a111a11b06f3288905a7f878f4f5f1a02fa2f
waj949 <wajdihamrouni1993@gmail.com>
2020-05-20 04:38:27 +0100

    deployed in heroku

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  console.log("hello");
+  res.send("Working");
+});
+
+app.listen(3001, () => {
+  console.log(" app is listening on 3001");
+});
